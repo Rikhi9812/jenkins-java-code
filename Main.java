88153815bcc629fa52e1f57a335ee6c566545d5a
Rikhi9812 <rishi.rana606@gmail.com>
2023-09-05 17:45:55 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is  a jva program");
+        System.out.println("Test me");
+        System.out.println(new Date());
+    }
+}
