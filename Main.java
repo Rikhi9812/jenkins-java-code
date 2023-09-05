6d4ede12cb28eff02b362754324d0b89fa6d6f1e
Rikhi9812 <rishi.rana606@gmail.com>
2023-09-05 17:47:03 +0900

    init

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is  a jva program");
         System.out.println("Test me");
+        System.out.println("Hello world");
         System.out.println(new Date());
     }
 }
